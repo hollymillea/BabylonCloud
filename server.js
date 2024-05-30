e7fb723926ada77e6331901259cffb9313e32260
Holly Millea <hollymillea@gmail.com>
2024-05-30 11:21:55 +0100

    Getting rid of unnecessary code

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ const app = express();
 // Use routes (i.e., connect the routes to the server)
 app.use('/example', exampleRoute);
 
-// Serve static files (your front-end build)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public'))); // assuming your built front-end files are in the 'public' folder
-
 // Set up and listen to a port 
 const port = 5173;
 app.listen(port, () => {
